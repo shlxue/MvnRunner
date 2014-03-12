06c2b4c3496f18e1e8ddc6f5cdca15776c7d7439
Xue Shuiliang <XueShuiliang@gmail.com>
2014-03-13 00:28:42 +0800

    include non project pom files

--- src/main/java/com/lightd/ideap/maven/navigation/PomNavigationContributor.java
+++ src/main/java/com/lightd/ideap/maven/navigation/PomNavigationContributor.java
@@ -4,23 +4,32 @@ import com.intellij.navigation.ChooseByNameContributorEx;
 import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiFileSystemItem;
 import com.intellij.psi.PsiManager;
+import com.intellij.psi.codeStyle.MinusculeMatcher;
+import com.intellij.psi.codeStyle.NameUtil;
+import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.GlobalSearchScopes;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.CommonProcessors;
 import com.intellij.util.Processor;
 import com.intellij.util.indexing.FindSymbolParameters;
 import com.intellij.util.indexing.IdFilter;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.idea.maven.dom.MavenDomUtil;
 import org.jetbrains.idea.maven.model.MavenId;
 import org.jetbrains.idea.maven.project.MavenProject;
 import org.jetbrains.idea.maven.project.MavenProjectsManager;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PomNavigationContributor implements ChooseByNameContributorEx, DumbAware {
     private final Project project;
+    private MavenProjectsManager projectsManager;
 
     public PomNavigationContributor(Project project) {
         this.project = project;
@@ -28,17 +37,23 @@ public class PomNavigationContributor implements ChooseByNameContributorEx, Dumb
 
     @Override
     public void processNames(@NotNull final Processor<String> processor, @NotNull GlobalSearchScope scope, IdFilter filter) {
-        List<MavenProject> mavenProjects = MavenProjectsManager.getInstance(project).getRootProjects();
+        projectsManager = MavenProjectsManager.getInstance(project);
+        List<MavenProject> mavenProjects = projectsManager.getRootProjects();
         if (mavenProjects.size() > 0) {
             addProjectNames(processor, mavenProjects.get(0));
+
+            if (scope.isSearchInLibraries()) {
+                for (MavenId mavenId : getNotImportPoms(mavenProjects.get(0)).keySet()) {
+                    processor.process(mavenId.getKey());
+                }
+            }
         }
     }
 
-    private void addProjectNames(final Processor<String> processor, MavenProject rootProject) {
-        MavenId id = rootProject.getMavenId();
-        processor.process(String.format("%s:%s:%s", id.getGroupId(), id.getArtifactId(), id.getVersion()));
-        for (MavenProject mavenProject : MavenProjectsManager.getInstance(project).getModules(rootProject)) {
-            addProjectNames(processor, mavenProject);
+    private void addProjectNames(final Processor<String> processor, MavenProject mavenProject) {
+        processor.process(mavenProject.getMavenId().getKey());
+        for (MavenProject subModule : projectsManager.getModules(mavenProject)) {
+            addProjectNames(processor, subModule);
         }
     }
 
@@ -46,20 +61,24 @@ public class PomNavigationContributor implements ChooseByNameContributorEx, Dumb
     public void processElementsWithName(@NotNull String name,
                                         @NotNull final Processor<NavigationItem> processor,
                                         @NotNull FindSymbolParameters parameters) {
-        String[] names = name.split(":");
-        if (!StringUtil.isEmptyOrSpaces(parameters.getCompletePattern())) {
-            String[] pattern = parameters.getCompletePattern().split(":");
+        final MinusculeMatcher matcher = NameUtil.buildMatcher(parameters.getCompletePattern(), NameUtil.MatchingCaseSensitivity.FIRST_LETTER);
 
-            if (pattern.length == 1 && !name.contains(pattern[0]) ||
-                    pattern.length > 1 && (!StringUtil.containsIgnoreCase(names[0], pattern[0]) ||
-                            !StringUtil.containsIgnoreCase(names[1], pattern[1]))) {
-                return;
-            }
-        }
+        if (!matcher.isStartMatch(name.substring(0, name.lastIndexOf(':')))) return;
+
+        String[] names = name.split(":");
         MavenId mavenId = new MavenId(names[0], names[1], names[2]);
-        MavenProject p = MavenProjectsManager.getInstance(project).findProject(mavenId);
+        projectsManager = MavenProjectsManager.getInstance(project);
+        MavenProject p = projectsManager.findProject(mavenId);
+        PsiFileSystemItem pomFile = null;
         if (p != null) {
-            processor.process(new PomWrapper(PsiManager.getInstance(project).findFile(p.getFile()), mavenId, project.getBasePath()));
+            pomFile = PsiManager.getInstance(project).findFile(p.getFile());
+        } else if (parameters.isSearchInLibraries()) {
+            Map<MavenId, PsiFile> notImportPoms = getNotImportPoms(projectsManager.getRootProjects().get(0));
+            pomFile = notImportPoms.get(mavenId);
+        }
+        if (pomFile != null) {
+            PomWrapper pomWrapper = new PomWrapper(pomFile, mavenId, project.getBasePath());
+            processor.process(pomWrapper);
         }
     }
 
@@ -78,5 +97,18 @@ public class PomNavigationContributor implements ChooseByNameContributorEx, Dumb
         CommonProcessors.CollectProcessor<NavigationItem> processor = new CommonProcessors.CollectProcessor<NavigationItem>();
         processElementsWithName(name, processor, FindSymbolParameters.wrap(pattern, project, includeNonProjectItems));
         return processor.toArray(new NavigationItem[processor.getResults().size()]);
+    }
+
+    private Map<MavenId, PsiFile> getNotImportPoms(MavenProject mavenProject) {
+        GlobalSearchScope pomScope = GlobalSearchScopes.directoryScope(project, mavenProject.getDirectoryFile(), true);
+        PsiFile[] psiFiles = FilenameIndex.getFilesByName(project, "pom.xml", pomScope);
+        Map<MavenId, PsiFile> poms = new LinkedHashMap<MavenId, PsiFile>(psiFiles.length);
+        for (PsiFile psiFile : psiFiles) {
+            if (projectsManager.findProject(psiFile.getVirtualFile()) == null) {
+                MavenId mavenId = MavenDomUtil.describe(psiFile);
+                poms.put(mavenId, psiFile);
+            }
+        }
+        return poms;
     }
 }
--- src/main/java/com/lightd/ideap/maven/navigation/PomWrapper.java
+++ src/main/java/com/lightd/ideap/maven/navigation/PomWrapper.java
@@ -1,23 +1,29 @@
 package com.lightd.ideap.maven.navigation;
 
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import icons.MavenIcons;
+import com.intellij.psi.PsiFileSystemItem;
+import com.intellij.psi.impl.RenameableFakePsiElement;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.idea.maven.dom.references.MavenPsiElementWrapper;
 import org.jetbrains.idea.maven.model.MavenCoordinate;
 
 import javax.swing.*;
 
-public class PomWrapper extends MavenPsiElementWrapper {
+public class PomWrapper extends RenameableFakePsiElement {
     private final MavenCoordinate coordinate;
     private final String rootPath;
+    private final PsiFileSystemItem pomFile;
 
-    PomWrapper(PsiElement wrappeeElement, MavenCoordinate coordinate, String rootPath) {
-        super(wrappeeElement, null);
+    PomWrapper(PsiFileSystemItem pomFile, MavenCoordinate coordinate, String rootPath) {
+        super(pomFile.getParent());
+        this.pomFile = pomFile;
         this.coordinate = coordinate;
         this.rootPath = rootPath;
+    }
+
+    @Override
+    public String getTypeName() {
+        return pomFile.getVirtualFile().getFileType().getName();
     }
 
     @Override
@@ -27,22 +33,17 @@ public class PomWrapper extends MavenPsiElementWrapper {
 
     @Override
     public Icon getIcon() {
-        return MavenIcons.MavenLogo;
-    }
-
-    @Override
-    public PsiFile getWrappee() {
-        return (PsiFile) super.getWrappee();
+        return pomFile.getIcon(ICON_FLAG_VISIBILITY);
     }
 
     @Nullable
     @Override
     public String getLocationString() {
         String localPath = null;
-        if (getWrappee().getPresentation() != null) {
-            localPath = getWrappee().getPresentation().getLocationString();
+        if (pomFile.getPresentation() != null) {
+            localPath = pomFile.getPresentation().getLocationString();
             if (localPath != null && localPath.length() > rootPath.length()) {
-                return "." + localPath.substring(rootPath.length());
+                localPath = "." + localPath.substring(rootPath.length());
             }
         }
         return localPath;
@@ -50,15 +51,18 @@ public class PomWrapper extends MavenPsiElementWrapper {
 
     @Override
     public boolean isValid() {
-        return getWrappee().isValid();
+        return pomFile.isValid();
     }
 
     @Override
     public PsiFile getContainingFile() {
-        return getWrappee().getContainingFile();
+        if (pomFile instanceof PsiFile) {
+            return (PsiFile) pomFile;
+        }
+        return pomFile.getContainingFile();
     }
 
     public VirtualFile getVirtualFile() {
-        return getWrappee().getVirtualFile();
+        return pomFile.getVirtualFile();
     }
 }
